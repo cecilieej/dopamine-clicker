2ba374d7ade91df733d97ce3f69e9ba7e046a7d8
Cecilie <cjacob20@student.aau.dk>
2025-09-25 01:12:49 +0200

    Update counter text

--- clicker/src/components/Counter.jsx
+++ clicker/src/components/Counter.jsx
@@ -1,5 +1,5 @@
 function Counter({ dopamine }) {
-  return <h1 className="counter">Dopamine: {Math.floor(dopamine)}</h1>;
+  return <h1 className="counter">You have {Math.floor(dopamine)} dopamine</h1>;
 }
 
 export default Counter;
