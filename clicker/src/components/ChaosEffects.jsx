35c480c5935a408da7ee06c2fc9fb6de00584fc9
Cecilie <cjacob20@student.aau.dk>
2025-09-25 00:38:41 +0200

    Notes to make ChaosEffects.jsx a future TODO

--- clicker/src/components/ChaosEffects.jsx
+++ clicker/src/components/ChaosEffects.jsx
@@ -1,5 +1,6 @@
+// Currently set on hold until I decide if I even want this 
 export default function ChaosEffects({ chaosLevel }) {
-  // TODO: Scale chaosLevel to simple effects (maybe can add CSS filters, sounds, popups, etc.)
+  // TODO??: Scale chaosLevel to simple effects (maybe can add CSS filters, sounds, popups, etc.)
   const chaosStyle = {
     filter: `hue-rotate(${chaosLevel * 10}deg)`,
   };
